0b90bfb37656d2d4bae426520360895b7815ef24
Olena Tsybulska <tsybulskayallena@gmail.com>
2021-05-17 22:53:24 +0300

    feat redux, useDispatch(), useSelector()

--- #src/components/App.jsx
+++ #src/components/App.jsx
@@ -1,10 +1,20 @@
 import './app.less'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setCount } from '../reducers/reposReducer'
 
 const App = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(state => state.repos.count)
+
+    function onCountClick() {
+        dispatch(setCount(5))
+    }
+
     return (
         <div className='app'>
-            React is working!
+            <button onClick={()=>onCountClick()}>COUNT</button>
+            <div>{count}</div>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
     devServer: {
         port: 3000,
     },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     plugins: [
         new HTMLWebpackPlugin({template: './#src/index.html'}),
         new CleanWebpackPlugin(),
