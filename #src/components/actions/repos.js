418314cd13e14301f648f9d3908379f705d8ea65
Olena Tsybulska <tsybulskayallena@gmail.com>
2021-05-20 18:30:32 +0300

    feat search, loading

--- #src/components/actions/repos.js
+++ #src/components/actions/repos.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import { setRepos } from '../../reducers/reposReducer'
+import { setRepos, setIsFetching } from '../../reducers/reposReducer'
 
-export const getRepos = (searchQuery = "stars:%3E1") => {
+export const getRepos = (searchQuery = 'stars:%3E1') => {
+    if (searchQuery == '') searchQuery = 'stars:%3E1'
     return async (dispatch) => {
+        dispatch(setIsFetching(true))
         const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`)
         dispatch(setRepos(response.data))
     }
 }
-
-export default getRepos;
--- #src/components/main/repo/Repo.jsx
+++ #src/components/main/repo/Repo.jsx
@@ -8,10 +8,11 @@ const Repo = (props) => {
         <div className="repo" key="props.id">
             <div className="repo__header">
                 <div className="repo__header-name">Repository name: {repo.name}</div>
-                <div className="repo__header-stars">Number of stars: {repo.stargazers_count}</div>
+                <div className="repo__header-description">Description: {repo.description}</div>
             </div>
             <div className="repo__other">
-                <div className="repo__last-commit">Date of last update: {repo.updated_at}</div>
+                <div className="repo__stars">Number of stars: {repo.stargazers_count}</div>
+                <div className="repo__last-commit">Date of last update: {repo.updated_at.substring(0, repo.updated_at.length - 10)}</div>
                 <a href={repo.html_url} className="repo__link">Repository link</a>
             </div>
         </div>
